6d8cc60d4b1eac7e8cd76dd1075d090cbdb9d36d
Amaan <patelspune@gmail.com>
2026-04-21 12:16:25 +0530

    Serve frontend

--- server/server.js
+++ server/server.js
@@ -104,3 +104,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () =>
   console.log(`🚀 Server running on http://0.0.0.0:${PORT} (LAN accessible)`)
 );
+import path from "path";
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
